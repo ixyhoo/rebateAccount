0c3efe37ae5ec5b4961200955a9b5713c532ab56
ixyhoo <ixyhoo@outlook.fr>
2023-06-01 10:53:57 +0200

    new rebate account

--- index.js
+++ index.js
@@ -8,14 +8,33 @@
 //let seconds = totalSeconds % 60
 
 // Créer un événement à la validation du form pour lancer le compte à rebours
-let totalSeconds = choice.value * 60
-let countdown = setInterval(function() {
-    if (totalSeconds <= 0) {
-        clearInterval(countdown)
-        console.log("Time's up!");
-        return;
+const form = document.querySelector('#form');
+const choice = document.querySelector('#choice');
+const countdownDisplay = document.querySelector('#countdownDisplay');
+
+let countdown = null; // déclarer countdown ici
+
+form.addEventListener('submit', function(e) {
+  e.preventDefault(); 
+
+  if (countdown) {
+    clearInterval(countdown); // arrêter le compte à rebours précédent
+  }
+
+  let totalSeconds = choice.value * 60;
+
+  countdown = setInterval(() => { // assigner le nouveau compte à rebours à countdown
+    if (totalSeconds < 0) {
+      clearInterval(countdown);
+      countdownDisplay.innerHTML = "Countdown Finished";
+      return;
     }
+
     let minutes = Math.floor(totalSeconds / 60);
     let seconds = totalSeconds % 60;
-    console.log(`Time remaining: ${minutes}:${seconds < 10 ? '0' : ''}${seconds}`);
-}, 1000)
+
+    countdownDisplay.innerHTML = `Time remaining: ${minutes}:${seconds < 10 ? '0' : ''}${seconds}`;
+    totalSeconds--;
+  }, 1000);
+});
+
