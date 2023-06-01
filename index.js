7ad848f4802722927c361061605d56556106cdef
ixyhoo <ixyhoo@outlook.fr>
2023-06-01 10:47:12 +0200

    change body background

--- index.js
+++ index.js
@@ -1,10 +1,21 @@
 // Créer la logique de compte à rebours
 
-choice.value = 5 minutes
+//choice.value = 5 minutes
 
-choice.value * 60 = totalSeconds
+//hoice.value * 60 = totalSeconds
 
-let minutes = Math.floor(totalSeconds / 60);
-let seconds = totalSeconds % 60
+//let minutes = Math.floor(totalSeconds / 60);
+//let seconds = totalSeconds % 60
 
 // Créer un événement à la validation du form pour lancer le compte à rebours
+let totalSeconds = choice.value * 60
+let countdown = setInterval(function() {
+    if (totalSeconds <= 0) {
+        clearInterval(countdown)
+        console.log("Time's up!");
+        return;
+    }
+    let minutes = Math.floor(totalSeconds / 60);
+    let seconds = totalSeconds % 60;
+    console.log(`Time remaining: ${minutes}:${seconds < 10 ? '0' : ''}${seconds}`);
+}, 1000)
